25752d79ef1824b4bdd811bcc1fa0a6babc1b2a3
Cesar Romero <cesarromero34@gmail.com>
2026-05-30 14:09:28 -0300

    feat: remove o uso do cron e transforma o scrapJob em chamada das principais funções da aplicação

--- src/jobs/scrapeJob.ts
+++ src/jobs/scrapeJob.ts
@@ -1,31 +1,21 @@
-import cron from "node-cron";
-import { checkAllProducts } from "../services/priceService";
 import LoggerHelper from "../utils/logger";
-import { ENVIRONMENT } from "../environment";
+import { connectDB } from "../utils/database";
+import { addProduct, checkAllProducts } from "../services/priceService";
 
-export function startScrapeJob(logger: LoggerHelper): void {
-  if (!cron.validate(ENVIRONMENT.executionSchedule!)) {
-    logger.log(
-      "ERROR",
-      `CRON_SCHEDULE inválido: "${ENVIRONMENT.executionSchedule!}"`,
+export async function startScrapeJob(logger: LoggerHelper): Promise<void> {
+  try {
+    await connectDB(logger);
+
+    await addProduct(
+      "Morte Subita - Mascara Capilar",
+      "mascara-de-hidrataco-morte-subita-450g-lola-cosmetics/p/MLB19485497?pdp_filters=item_id%3AMLB2752028221",
+      30,
+      logger,
     );
-    process.exit(1);
+
+    await checkAllProducts(logger);
+  } catch (e) {
+    const err = e as Error;
+    logger.log("ERROR", `Erro ao iniciar o scraping: ${err.message}`);
   }
-
-  logger.log(
-    "INFO",
-    `Job de scraping agendado: "${ENVIRONMENT.executionSchedule!}"`,
-  );
-
-  cron.schedule(ENVIRONMENT.executionSchedule!, async () => {
-    logger.log("INFO", "Executando job de scraping...");
-    try {
-      await checkAllProducts(logger);
-      logger.log("INFO", "Job finalizado com sucesso.");
-      return true;
-    } catch (err) {
-      logger.log("ERROR", "Erro no job de scraping:" + err);
-      return false;
-    }
-  });
 }
