d2709f8643891d46c1b0df1a588ec1de803cf21a
Cesar Romero <cesarromero34@gmail.com>
2026-05-30 14:09:56 -0300

    feat: implementa validação de possíveis erros ao realizar o scrapping do mercado livre

--- src/scrapers/mercadoLivreScraper.ts
+++ src/scrapers/mercadoLivreScraper.ts
@@ -17,6 +17,7 @@ function parsePrice(text: string | null): number | null {
 
 export async function scrapeMercadoLivre(
   url: string,
+  productName: string,
   logger: LoggerHelper,
 ): Promise<ScrapeResult> {
   const browser = await chromium.launch(browserConfig);
@@ -28,8 +29,49 @@ export async function scrapeMercadoLivre(
   const page = await context.newPage();
 
   try {
+    let error = {
+      happened: false,
+      errorMessage: "",
+    };
     logger.log("INFO", `Scraping: ${url}`);
     await page.goto(url, { waitUntil: "domcontentloaded", timeout: 30_000 });
+
+    try {
+      const catpchaCheck = page.getByRole("heading", {
+        name: "Por segurança, complete esta",
+      });
+      await catpchaCheck.waitFor({ state: "visible", timeout: 7000 });
+      logger.log("INFO", "Captcha encontrado. Finalizando processo.");
+      error = {
+        happened: true,
+        errorMessage: "Captcha interrompeu o processo",
+      };
+    } catch (e) {
+      logger.log(
+        "INFO",
+        "Captcha nao encontrado. Prosseguindo com o scrapping.",
+      );
+    }
+
+    try {
+      const loginCheck = page.getByText("Olá! Para continuar,");
+      await loginCheck.waitFor({ state: "visible", timeout: 7000 });
+      logger.log("INFO", "Login solicitado. Finalizando processo.");
+
+      error = {
+        happened: true,
+        errorMessage: "Solicitação de login interrompeu o processo",
+      };
+    } catch (e) {
+      logger.log(
+        "INFO",
+        "Solicitação de login nao encontrado. Prosseguindo com o scrapping.",
+      );
+    }
+
+    if (error.happened) {
+      throw new Error(error.errorMessage);
+    }
 
     const title = await page
       .locator("h1.ui-pdp-title")
@@ -41,22 +83,36 @@ export async function scrapeMercadoLivre(
 
     // Tenta pegar o preço com desconto primeiro
     try {
-      const discountLocator = page
-        .locator(".ui-pdp-price__second-value .andes-money-amount__fraction")
-        .first();
-      await discountLocator.waitFor({ state: "visible", timeout: 7000 });
-      price = parsePrice(await discountLocator.textContent());
-      logger.log("INFO", `Preço com desconto encontrado: ${price}`);
-    } catch {
-      // Fallback: preço normal
-      logger.log(
-        "INFO",
-        "Preço com desconto não encontrado, usando preço normal.",
+      await page.pause();
+      const priceArea = page.locator(
+        "div.ui-pdp-container__row.ui-pdp-container__row--price",
       );
-      const normalLocator = page
-        .locator(".ui-pdp-price__main-value .andes-money-amount__fraction")
-        .first();
-      price = parsePrice(await normalLocator.textContent().catch(() => null));
+      await priceArea.waitFor({ state: "visible", timeout: 7000 });
+      const priceAreaText = await priceArea.textContent();
+      logger.log("INFO", `Conteúdo da área de preço: ${priceAreaText}`);
+
+      const [, fullPrice, discountPrice] = priceAreaText
+        ? priceAreaText.split("R$")
+        : ["", "", ""];
+      const discount = discountPrice.slice(5, 8);
+
+      // 'R$49,90Preço por litro: R$110,89Ver os meios de pagamento'
+      // R$59,90R$36,9838% OFFPreço por litro: R$82,18Ver os meios de pagamento
+
+      if (priceAreaText?.includes("OFF")) {
+        logger.log(
+          "INFO",
+          `Desconto de ${discount} encontrado, pegando preço com desconto.`,
+        );
+        price = parsePrice(discountPrice.slice(0, 5));
+      } else {
+        logger.log("INFO", "Desconto não encontrado, pegando preço normal.");
+        price = parsePrice(fullPrice);
+      }
+      logger.log("INFO", `Preço: ${price}`);
+    } catch (e) {
+      let error = e as Error;
+      logger.log("ERROR", `Erro ao encontrar preço: ${error.message}`);
     }
 
     const unavailable = await page
@@ -71,7 +127,7 @@ export async function scrapeMercadoLivre(
       available: !unavailable && price !== null,
     };
   } catch (error) {
-    logger.log("ERROR", `Erro ao fazer scraping de ${url}: ${error}`);
+    logger.log("ERROR", `Erro ao fazer scraping de ${productName}: ${error}`);
     return { title: "Erro", price: null, available: false };
   } finally {
     await browser.close();
