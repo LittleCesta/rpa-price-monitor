c96b81abbd2dba0e01afc7c45a901dd8e2c750ce
Cesar Romero <cesarromero34@gmail.com>
2026-05-30 22:23:50 -0300

    chore: ajusta mensagem e preço alvo

--- src/jobs/scrapeJob.ts
+++ src/jobs/scrapeJob.ts
@@ -1,19 +1,18 @@
 import LoggerHelper from "../utils/logger";
-import { connectDB } from "../utils/database";
 import { addProduct, checkAllProducts } from "../services/priceService";
 
 export async function startScrapeJob(logger: LoggerHelper): Promise<void> {
   try {
-    await connectDB(logger);
-
     await addProduct(
       "Morte Subita - Mascara Capilar",
       "mascara-de-hidrataco-morte-subita-450g-lola-cosmetics/p/MLB19485497?pdp_filters=item_id%3AMLB2752028221",
-      30,
+      40,
       logger,
     );
 
     await checkAllProducts(logger);
+
+    logger.log("INFO", "Scraping finalizado.");
   } catch (e) {
     const err = e as Error;
     logger.log("ERROR", `Erro ao iniciar o scraping: ${err.message}`);
